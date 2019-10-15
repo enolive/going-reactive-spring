fefe8006bb27dccef0ddaa6ee7d209f45f23fbdd
Christoph Welcz <christoph@welcz.de>
2019-10-15 14:26:42 +0200

    changed bootstrapping so it creates a new collection always

--- demo/give-hugs/backend/src/main/java/de/welcz/givehugsbackend/CreateDb.java
+++ demo/give-hugs/backend/src/main/java/de/welcz/givehugsbackend/CreateDb.java
@@ -2,13 +2,11 @@ package de.welcz.givehugsbackend;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.core.CollectionOptions;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile("bootstrap")
 public class CreateDb implements CommandLineRunner {
   private final MongoOperations db;
 
@@ -19,7 +17,9 @@ public class CreateDb implements CommandLineRunner {
 
   @Override
   public void run(String... args) {
-    db.dropCollection("hugs");
+    if (db.collectionExists("hugs")) {
+      return;
+    }
     final var options = CollectionOptions.empty()
                                          .capped()
                                          .size(8196)
